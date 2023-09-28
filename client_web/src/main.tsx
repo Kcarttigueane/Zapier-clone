c510570c9016d09a155ed06f152d126549345709
Jules DUTEL <jules.dutel@epitech.eu>
2023-09-28 17:02:19 +0200

    Revert "add(web):add demo page and component footer"
    This reverts commit 2cead35495b7abca84c06cb28b794a3d39cb1f9a.

--- client_web/src/main.tsx
+++ client_web/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DemoPage from './DemoPage.tsx';
+import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<DemoPage></DemoPage>
+		<App />
 	</React.StrictMode>,
 );
