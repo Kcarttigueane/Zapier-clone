e72e1e9d42c71ede13c5a9e79a4384ed1a0f6658
Yann Demuyt <demisistired@demissexroom.home>
2023-10-04 10:50:51 +0200

    feat(server): Added middleware

--- server/main.py
+++ server/main.py
@@ -1,14 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from config.database import close_mongo_connection, connect_to_mongo
 from routers.auth import auth_router
 from routers.user import users_router
 from routers.services_spotify import services_router_spotify
 from routers.services_google import services_router_google
+from config.constants import CLIENT_URL
 from routers.services_discord import services_router_discord
 
 from routers.automations import automation_router
 
 app = FastAPI()
+
+origins = [
+    CLIENT_URL,
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event():
