32cfab6c0f55bdfb4ce281c04e11ad11bdcfe94c
KC <carttigueane.k@hotmail.com>
2023-10-04 10:37:51 +0200

    feat(web/i18n): Add spanish translations

--- client_web/src/i18n/i18next.tsx
+++ client_web/src/i18n/i18next.tsx
@@ -1,11 +1,13 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import basics_en from './en/en.json';
+import basics_es from './es/es.json';
 import basics_fr from './fr/fr.json';
 
 const RESSOURCES = {
 	en: { translation: basics_en },
 	fr: { translation: basics_fr },
+	es: { translation: basics_es },
 };
 
 i18n.use(initReactI18next).init({
