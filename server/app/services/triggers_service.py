5b5fcf01115f90571c0a8288b5b2ce35a86f4466
KC <carttigueane.k@hotmail.com>
2023-10-22 00:52:26 +0200

    feat(trigger-action-service): add methods to fetch related triggers and actions
    - Create a new service class  with methods to fetch triggers related to a given action and actions related to a given trigger, while ensuring they are associated with a specified service.
    - Add a new repository class  to interact with the  collection in the database.
    - Implement  and  methods in  to fetch the compatibility documents based on action or trigger IDs respectively.

--- server/app/services/triggers_service.py
+++ server/app/services/triggers_service.py
@@ -82,3 +82,17 @@ class TriggersService:
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
             ) from e
+
+    async def get_triggers_by_action(
+        self, action_id: PyObjectId, service_id: PyObjectId
+    ) -> List[TriggerOutDTO]:
+        trigger_action_compatibilities = await self.repository.find_by_action_id(
+            action_id
+        )
+
+        triggers = []
+        for compatibility in trigger_action_compatibilities:
+            trigger = await TriggerRepository().get(compatibility.trigger_id)
+            if trigger and str(trigger.service_id) == str(service_id):
+                triggers.append(trigger)
+        return triggers
