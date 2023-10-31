fb08b1435e69bb47d015f4e658acc5a0ad49e58b
KC <carttigueane.k@hotmail.com>
2023-10-31 16:01:32 +0100

    feat(models)

---
+++ client_mobile/app/core/models/action.ts
@@ -0,0 +1,6 @@
+export interface ActionModelDTO {
+  id: string;
+  service_id: string;
+  name: string;
+  description: string;
+}
---
+++ client_mobile/app/core/models/user.ts
@@ -0,0 +1,22 @@
+interface UserProfileDTO {
+  first_name?: string;
+  last_name?: string;
+  language: string;
+  theme: 'dark' | 'light' | 'system';
+}
+
+enum UserRole {
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+}
+
+export interface UserModelDTO {
+  id: string;
+  email: string;
+  status: 'active' | 'inactive' | 'suspended';
+  role: UserRole;
+  email_verified: boolean;
+  profile: UserProfileDTO;
+  created_at: Date;
+  updated_at: Date;
+}
