0af8764f3cd2e3f23871b079849981fc3d1d1e0e
Yann Demuyt <demisistired@demissexroom.home>
2023-10-31 11:43:08 +0100

    feat(automation/trigger/youtube/new-video): Added Youtube Trigger for when user's subsribed channel uploads a new video.

--- server/app/source/triggers/google_youtube.py
+++ server/app/source/triggers/google_youtube.py
@@ -1,4 +1,5 @@
 import logging
+from typing import List
 from googleapiclient.discovery import build
 from datetime import datetime
 
@@ -32,6 +33,101 @@ def check_youtube_like(user: UserOutDTO, last_polled: datetime) -> TriggerAnswer
     try:
         return extract_youtube_likes(credentials)
 
+    except Exception as e:
+        logger.info(f"An error occurred: {e}")
+        return None
+
+
+def get_subscribed_channels(service, last_polled):
+    subscribed_channels = []
+
+    response = (
+        service.subscriptions()
+        .list(part="snippet", mine=True, maxResults=5, pageToken=None)
+        .execute()
+    )
+
+    subscribed_channels.extend(response["items"])
+
+    return subscribed_channels
+
+
+def get_latest_videos(service, last_polled, channels):
+    latest_videos: List[dict] = []
+
+    for channel in channels:
+        channel_id = channel["snippet"]["resourceId"]["channelId"]
+        response = (
+            service.search()
+            .list(
+                part="snippet",
+                channelId=channel_id,
+                maxResults=5,
+                order="date",
+                type="video",
+                publishedAfter=f"{last_polled.isoformat()}Z",
+            )
+            .execute()
+        )
+
+        if response["items"]:
+            channel_title = channel["snippet"]["title"]
+            latest_videos.extend(
+                {
+                    "channel_title": channel_title,
+                    "video_title": video["snippet"]["title"],
+                    "video_url": f'https://www.youtube.com/watch?v={video["id"]["videoId"]}',
+                }
+                for video in response["items"]
+            )
+
+    return latest_videos
+
+
+def create_body_str(videos):
+    body = "📢 New Videos Alert! 🎥\n\nYou have new videos from your subscribed channels:\n\n"
+
+    for index, video in enumerate(videos, start=1):
+        body += f"{index}. {video['channel_title']}\n"
+        body += f"   - Title: {video['video_title']}\n"
+        body += f"   - Watch Now: {video['video_url']}\n\n"
+
+    body += "Don't miss out on the latest content from your favorite channels! Click the links above to watch these new videos."
+
+    return body
+
+
+def extract_new_videos(credentials, last_polled) -> TriggerAnswer | None:
+    service = build("youtube", "v3", credentials=credentials, cache_discovery=False)
+    channels = get_subscribed_channels(service, last_polled)
+    print(f"Channels: {channels}")
+    if channels == []:
+        return None
+    videos = get_latest_videos(service, last_polled, channels)
+    print(f"Videos: {videos}")
+    if videos == []:
+        return None
+    body = create_body_str(videos)
+    print(body)
+    return TriggerAnswer(
+        objs=videos,
+        header="[Area] Your Subscribed Channels Have New Content",
+        body=body,
+    )
+
+
+def check_new_videos(user: UserOutDTO, last_polled: datetime) -> TriggerAnswer | None:
+    if (
+        service_auth := get_service_auth(user, "youtube")
+    ) and service_auth.refresh_token:
+        credentials = get_google_credentials(
+            service_auth.access_token, service_auth.refresh_token
+        )
+    else:
+        return None
+
+    try:
+        return extract_new_videos(credentials, last_polled)
     except Exception as e:
         logger.info(f"An error occurred: {e}")
         return None
