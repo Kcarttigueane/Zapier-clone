6d28d791edd3be25ac3d8ff687c0bbbf5002006f
Dylan Winter <dylan@airm215ssideral.home>
2023-11-03 19:46:50 +0100

    feat(unit-tests): origin * -> true

--- server/app/main.py
+++ server/app/main.py
@@ -28,7 +28,7 @@ origins = WEB_CLIENT_URL
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=True,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
